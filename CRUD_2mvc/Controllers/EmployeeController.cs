829cbd411172fa4de2484b4c3f2aa094c36c0647
sonal7516 <singhsonalb101096@gmail.com>
2022-02-25 12:31:26 +0530

    Add project files.

---
+++ CRUD_2mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD_2mvc.Models;
+
+namespace CRUD_2mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext DbContext;
+        public EmployeeController(ApplicationDbContext DbContext)
+        {
+            this.DbContext = DbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Employee> emp = DbContext.Employees.ToList();
+
+            return View(emp);
+        }
+
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            DbContext.Employees.Add(emp);
+            DbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var emp = DbContext.Employees.SingleOrDefault(e => e.Id == id);
+            if (emp!=null)
+            {
+                DbContext.Employees.Remove(emp);
+                DbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        
+        public IActionResult Edit(int id)
+        {
+            var emp = DbContext.Employees.SingleOrDefault(e => e.Id == id);
+
+            return View(emp);
+        }
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            DbContext.Employees.Update(emp);
+            DbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+            
+    }
+}
---
+++ CRUD_2mvc/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_2mvc.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public int Mobile { get; set; }
+
+    }
+}
